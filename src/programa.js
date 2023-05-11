8a77e305d719ef8cb974ec4547af32ad9c4c12b3
anattolia <anattolia@gmail.com>
2023-05-11 15:00:54 -0500

    sigue el poema

--- src/programa.js
+++ src/programa.js
@@ -8,73 +8,56 @@ const is = document.getElementsByClassName('i');
 const os = document.getElementsByClassName('o');
 const us = document.getElementsByClassName('u');
 
+const bs = document.getElementsByClassName('b');
+const cs = document.getElementsByClassName('c');
+const ds = document.getElementsByClassName('d');
+const js = document.getElementsByClassName('j');
 const ls = document.getElementsByClassName('l');
+const ms = document.getElementsByClassName('m');
+const ns = document.getElementsByClassName('n');
 const ps = document.getElementsByClassName('p');
 const rs = document.getElementsByClassName('r');
 const ss = document.getElementsByClassName('s');
+const ts = document.getElementsByClassName('t');
+const vs = document.getElementsByClassName('v');
+const zs = document.getElementsByClassName('z');
+
+function cambiarVocal(lista, letra) {
+  for (let i = 0; i < lista.length; i++) {
+    lista[i].addEventListener('mouseover', function () {
+      lista[i].classList.add('cambiada');
+      lista[i].innerText = `${letra}`;
+    });
+  }
+}
+
+function cambiarConsonante(lista, letra) {
+  for (let i = 0; i < lista.length; i++) {
+    lista[i].addEventListener('mouseover', function () {
+      lista[i].classList.add('consonante-cambiada');
+      lista[i].innerText = `${letra}`;
+    });
+  }
+}
 
 // Vocales
-for (let i = 0; i < as.length; i++) {
-  as[i].addEventListener('mouseover', function () {
-    as[i].classList.add('cambiada');
-    as[i].innerText = 'a';
-  });
-}
-
-for (let i = 0; i < es.length; i++) {
-  es[i].addEventListener('mouseover', function () {
-    es[i].classList.add('cambiada');
-    es[i].innerText = 'e';
-  });
-}
-
-for (let i = 0; i < is.length; i++) {
-  is[i].addEventListener('mouseover', function () {
-    is[i].classList.add('cambiada');
-    is[i].innerText = 'i';
-  });
-}
-
-for (let i = 0; i < os.length; i++) {
-  os[i].addEventListener('mouseover', function () {
-    os[i].classList.add('cambiada');
-    os[i].innerText = 'o';
-  });
-}
-
-for (let i = 0; i < us.length; i++) {
-  us[i].addEventListener('mouseover', function () {
-    us[i].classList.add('cambiada');
-    us[i].innerText = 'u';
-  });
-}
+cambiarVocal(as, 'a');
+cambiarVocal(es, 'e');
+cambiarVocal(is, 'i');
+cambiarVocal(os, 'o');
+cambiarVocal(us, 'u');
 
 // Consonantes
-
-for (let i = 0; i < ls.length; i++) {
-  ls[i].addEventListener('mouseover', function () {
-    ls[i].classList.add('consonante-cambiada');
-    ls[i].innerText = 'l';
-  });
-}
-
-for (let i = 0; i < ps.length; i++) {
-  ps[i].addEventListener('mouseover', function () {
-    ps[i].classList.add('consonante-cambiada');
-    ps[i].innerText = 'p';
-  });
-}
-
-for (let i = 0; i < rs.length; i++) {
-  rs[i].addEventListener('mouseover', function () {
-    rs[i].classList.add('consonante-cambiada');
-    rs[i].innerText = 'r';
-  });
-}
-
-for (let i = 0; i < ss.length; i++) {
-  ss[i].addEventListener('mouseover', function () {
-    ss[i].classList.add('consonante-cambiada');
-    ss[i].innerText = 's';
-  });
-}
+cambiarConsonante(bs, 'b');
+cambiarConsonante(cs, 'c');
+cambiarConsonante(ds, 'd');
+cambiarConsonante(js, 'j');
+cambiarConsonante(ls, 'l');
+cambiarConsonante(ms, 'm');
+cambiarConsonante(ns, 'n');
+cambiarConsonante(ps, 'p');
+cambiarConsonante(rs, 'r');
+cambiarConsonante(ss, 's');
+cambiarConsonante(ts, 't');
+cambiarConsonante(vs, 'v');
+cambiarConsonante(zs, 'z');
